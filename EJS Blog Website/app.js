65d7f25b2625cea6054e5c61ab3768922deaf30a
ismaildemircan <ismaildemircann98@gmail.com>
2020-12-20 01:58:05 +0300

    Create compose page and fix home page

--- EJS Blog Website/app.js
+++ EJS Blog Website/app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+const blogPosts = [];
+
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -19,7 +21,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", { contentHome: homeStartingContent });
+  res.render("home", {
+    contentHome: homeStartingContent,
+    blogPosts: blogPosts,
+  });
+});
+
+app.get("/blogPosts/:postName", (req, res) => {
+  console.log(req.params);
 });
 
 app.get("/about", (req, res) => {
@@ -28,6 +37,19 @@ app.get("/about", (req, res) => {
 
 app.get("/contact", (req, res) => {
   res.render("contact", { contentContact: contactContent });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+  const blogPost = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+  blogPosts.push(blogPost);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
